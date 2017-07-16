232b4f5a7c5fd316121d2bae53e5423507090eb2
Sergey Rubtsov <rsv.astana@gmail.com>
2017-07-16 19:33:22 +0600

    Дополнил наполнении бд при создание

--- DataAccess/Impl/DbInitializer.cs
+++ DataAccess/Impl/DbInitializer.cs
@@ -15,7 +15,7 @@ namespace DataAccess.Impl
                 return;
             }
 
-            var checkIns = new CheckIn[]
+            var checkIns = new[]
             {
                 new CheckIn
                 {
@@ -23,10 +23,10 @@ namespace DataAccess.Impl
                     {
                         FirstName = "Пётр",
                         LastName = "Петров",
-                        BirthDate = new DateTime(1990, 9, 10),
+                        BirthDate = new DateTime(1990, 9, 15),
                         Sex = Sex.Male
                     },
-                    VisitDateTime = DateTime.Now
+                    VisitDateTime = new DateTime(2017, 07, 15, 12, 00, 00)
                 },
                 new CheckIn
                 {
@@ -34,10 +34,21 @@ namespace DataAccess.Impl
                     {
                         FirstName = "Анжелика",
                         LastName = "Андреева",
-                        BirthDate = new DateTime(1991, 7, 3),
+                        BirthDate = new DateTime(1991, 7, 16),
                         Sex = Sex.Female
                     },
-                    VisitDateTime = DateTime.Now
+                    VisitDateTime = new DateTime(2017, 07, 15, 13, 00, 00)
+                },
+                new CheckIn
+                {
+                    Person = new Person
+                    {
+                        FirstName = "Андрей",
+                        LastName = "Жилин",
+                        BirthDate = new DateTime(1992, 8, 29),
+                        Sex = Sex.Male
+                    },
+                    VisitDateTime = new DateTime(2017, 07, 16, 19, 00, 00)
                 }
             };
             context.CheckIns.AddRange(checkIns);
